2b50aaae6a55de2feb3d1083ce49456c7e04ad27
Navon Francis <navonfrancis@gmail.com>
2019-01-06 16:19:58 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import Home from './components/Home';
+
+const App = () => {
+  document.body.style.backgroundColor = "#282c34";
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
