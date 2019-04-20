839bfc3772948c8dce4449f1300b79668987fd2b
Navon Francis <navonfrancis@gmail.com>
2019-04-20 00:32:16 -0400

    a lot.. its late

--- src/components/BarMap.js
+++ src/components/BarMap.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Control from 'react-leaflet-control';
 import L from 'leaflet';
+import Fab from '@material-ui/core/Fab';
+import MyLocation from '@material-ui/icons/MyLocation';
+import Hamburger from '@material-ui/icons/Dehaze';
 
 const API_URL = "https://vast-woodland-33247.herokuapp.com";
 const personMarker = L.icon({ iconUrl : require("./../assets/man-waving-arm.png") });
@@ -15,11 +19,17 @@ export default class BarMap extends Component {
         lng: 0,
         zoom: 2,
       },
+      originalCoords: {
+        lat: 0,
+        lng: 0,
+        zoom: 2,
+      },
       barData: [],
     }
 
     this.getNearestBars = this.getNearestBars.bind(this);
     this.updatePositionMarker = this.updatePositionMarker.bind(this);
+    this.snapToOriginalPosition = this.snapToOriginalPosition.bind(this);
   }
 
   componentWillMount() {
@@ -30,6 +40,7 @@ export default class BarMap extends Component {
         zoom : 11,
       }
       this.setState({coords : tmpCoords});
+      this.setState({originalCoords : tmpCoords});
       console.log("Location Services Enabled.");
       this.getNearestBars(this.state.coords.lat, this.state.coords.lng);
     });
@@ -53,7 +64,7 @@ export default class BarMap extends Component {
     this.props.callBackFromMap(data_1);
   }
 
-  updatePositionMarker(e){
+  updatePositionMarker(e) {
     const latlng = e.target.getLatLng();
     const newCoords = {
       lat : latlng.lat,
@@ -64,28 +75,54 @@ export default class BarMap extends Component {
     this.getNearestBars(newCoords.lat, newCoords.lng);
   }
 
+  snapToOriginalPosition() {
+    const org = this.state.originalCoords;
+    this.setState({coords: org});
+    this.getNearestBars(org.lat, org.lng);
+  }
+
   render() {
     const position = [this.state.coords.lat, this.state.coords.lng]
+    // https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibmF2b25mIiwiYSI6ImNqdW9vdG84ZTMxaDAzeW5xcjd4Y28xdnAifQ.D_EM5YywMts0q7qDBQciVg
     // const mapUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    const mapUrl = "https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
-    // const mapUrl = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
+    // const mapUrl = "https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}"
+    const mapUrl = "https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibmF2b25mIiwiYSI6ImNqdW9vdG84ZTMxaDAzeW5xcjd4Y28xdnAifQ.D_EM5YywMts0q7qDBQciVg"
+
 
     return (
       <Map 
         style={styles.mapStyle} 
         center={position} 
-        zoom={this.state.coords.zoom}>
+        zoomControl={false}
+        zoom={this.state.coords.zoom}
+      >
+        <Control position="topleft" >
+          <Fab color={"primary"}>
+            <Hamburger />
+          </Fab>
+        </Control>
+        <Control position="topright" >
+          <Fab color={"secondary"} onClick={this.snapToOriginalPosition}>
+            <MyLocation />
+          </Fab>
+        </Control>
         <TileLayer
           attribution='Drink Responsibly :)'
           url={mapUrl}
         />
-      <Marker draggable={true} zIndexOffset={1000} icon={personMarker} position={position} onDragEnd={this.updatePositionMarker}>
-        <Popup autoPan={true}>
-          <center>
-            <span><b>me thirsty!</b></span>
-          </center>
-        </Popup>
-      </Marker>
+        <Marker 
+          draggable={true} 
+          zIndexOffset={1000} 
+          icon={personMarker} 
+          position={position} 
+          onDragEnd={this.updatePositionMarker}
+        >
+          <Popup autoPan={true}>
+            <center>
+              <span>me thirsty!</span>
+            </center>
+          </Popup>
+        </Marker>
       {
         this.state.barData.map((bar, idx) =>
           <Marker key={`marker-${idx}`} position={[bar.lat, bar.lng]}>
